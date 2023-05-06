68e9574752859f0173cc321f1e7db56c28e49830
Dale Bezolli <deilbezolli@gmail.com>
2023-05-06 13:56:52 +0300

    Move user from MainActivity to HomeActivity

--- app/src/main/java/com/example/shopmanager/MainActivity.java
+++ app/src/main/java/com/example/shopmanager/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.shopmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.shopmanager.Home.HomeActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent startActivityIntent = new Intent(this, HomeActivity.class);
+        startActivity(startActivityIntent);
+
+        finish();
     }
 }

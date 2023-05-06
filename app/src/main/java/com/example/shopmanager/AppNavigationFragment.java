db6f0216d28ef7f1ef05b7daa01b016727893123
Dale Bezolli <deilbezolli@gmail.com>
2023-05-06 15:47:09 +0300

    Change color of navigation depending on the activity

--- app/src/main/java/com/example/shopmanager/AppNavigationFragment.java
+++ app/src/main/java/com/example/shopmanager/AppNavigationFragment.java
@@ -1,10 +1,17 @@
 package com.example.shopmanager;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +25,41 @@ public class AppNavigationFragment extends Fragment {
 
     Button buttonHome, buttonSales, buttonStock;
 
+    BUTTON highlightedButton;
+
+    public static enum BUTTON { HOME, SALES, STOCK };
+
+    public AppNavigationFragment() {}
+
+    public void setHighlightButton(BUTTON button) {
+        if(getContext() == null) return;
+
+        switch (button) {
+            case HOME:
+                buttonHome.setBackgroundTintList(ContextCompat.getColorStateList(getContext(), R.color.teal_200));
+                break;
+            case SALES:
+                buttonSales.setBackgroundTintList(ContextCompat.getColorStateList(getContext(), R.color.teal_200));
+                break;
+            case STOCK:
+                buttonStock.setBackgroundTintList(ContextCompat.getColorStateList(getContext(), R.color.teal_200));
+                break;
+        }
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        Activity parentActivity = (Activity) context;
+        if(parentActivity instanceof HomeActivity) {
+            highlightedButton = BUTTON.HOME;
+        } else if(parentActivity instanceof SalesActivity) {
+            highlightedButton = BUTTON.SALES;
+        }else if(parentActivity instanceof StockActivity) {
+                highlightedButton = BUTTON.STOCK;
+            }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_app_navigation, container, false);
@@ -26,21 +68,11 @@ public class AppNavigationFragment extends Fragment {
         buttonSales = view.findViewById(R.id.fragment_appnavigation_button_sales);
         buttonStock = view.findViewById(R.id.fragment_appnavigation_button_stock);
 
-        buttonHome.setOnClickListener(viewButtonHome -> {
-            Intent intentStartActivity = new Intent(getContext(), HomeActivity.class);
-            startActivity(intentStartActivity);
-        });
+        buttonHome.setOnClickListener(viewButtonHome -> changeActivity(viewButtonHome, HomeActivity.class));
+        buttonSales.setOnClickListener(viewButtonSales -> changeActivity(viewButtonSales, SalesActivity.class));
+        buttonStock.setOnClickListener(viewButtonStock -> changeActivity(viewButtonStock, StockActivity.class));
 
-        buttonSales.setOnClickListener(viewButtonSales -> {
-            Intent intentStartActivity = new Intent(getContext(), SalesActivity.class);
-            startActivity(intentStartActivity);
-        });
-
-        buttonStock.setOnClickListener(viewButtonStock -> {
-            Intent intentStartActivity = new Intent(getContext(), StockActivity.class);
-            startActivity(intentStartActivity);
-        });
-
+        setHighlightButton(highlightedButton);
         return view;
     }
 

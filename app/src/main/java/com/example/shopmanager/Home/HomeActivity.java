ca13c4821e22a62a0f1edcad9537c528e335e367
Dale Bezolli <deilbezolli@gmail.com>
2023-05-08 19:00:24 +0300

    Bind sales analytics data to view elements

--- app/src/main/java/com/example/shopmanager/Home/HomeActivity.java
+++ app/src/main/java/com/example/shopmanager/Home/HomeActivity.java
@@ -1,18 +1,25 @@
 package com.example.shopmanager.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.shopmanager.MainActivity;
 import com.example.shopmanager.R;
+import com.example.shopmanager.Sales.SaleRecyclerViewAdapter;
+import com.example.shopmanager.Stocks.StockRecyclerViewAdapter;
 import com.example.shopmanager.Storage.Analytics.StockAnalytics;
 
 public class HomeActivity extends AppCompatActivity {
+    SaleRecyclerViewAdapter saleRecyclerViewAdapter;
+    StockRecyclerViewAdapter stockRecyclerViewAdapter;
 
+    RecyclerView salesRecyclerView;
+    RecyclerView stockRecyclerView;
     TextView analyticsSalesTotal;
-
     TextView analyticsSalesToday;
     TextView analyticsStockTotal;
     TextView analyticsCategoriesTotal;
@@ -33,5 +40,12 @@ public class HomeActivity extends AppCompatActivity {
         StockAnalytics stockAnalytics = MainActivity.stockDatabase.stockDao().getStockAnalytics();
         analyticsStockTotal.setText(String.valueOf(stockAnalytics.getStockTotal()));
         analyticsCategoriesTotal.setText(String.valueOf(stockAnalytics.getCategoriesTotal()));
+
+        salesRecyclerView = findViewById(R.id.activity_home_constraint_latestsales_recyclerview_sales);
+        stockRecyclerView = findViewById(R.id.activity_home_constraint_stock_recyclerview_stock);
+
+        saleRecyclerViewAdapter = new SaleRecyclerViewAdapter(this, MainActivity.sales);
+        salesRecyclerView.setAdapter(saleRecyclerViewAdapter);
+        salesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }

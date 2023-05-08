14dd29fe7cddba2914bbce1d9f3ff3a86b3a5837
Dale Bezolli <deilbezolli@gmail.com>
2023-05-08 23:23:36 +0300

    Adapt navigation colors in HomeActivity

--- app/src/main/java/com/example/shopmanager/AppNavigationFragment.java
+++ app/src/main/java/com/example/shopmanager/AppNavigationFragment.java
@@ -6,7 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +24,7 @@ import com.example.shopmanager.Stocks.StocksActivity;
 
 public class AppNavigationFragment extends Fragment {
 
+    ConstraintLayout root;
     ImageButton buttonHome, buttonSales, buttonStock;
     View indicatorHome, indicatorSales, indicatorStock;
 
@@ -34,8 +39,16 @@ public class AppNavigationFragment extends Fragment {
 
         switch (button) {
             case HOME:
-                buttonHome.setImageTintList(getContext().getColorStateList(R.color.blue_400));
+                buttonHome.setImageTintList(getContext().getColorStateList(R.color.neutral_100));
                 indicatorHome.setVisibility(View.VISIBLE);
+                indicatorHome.setBackgroundTintList(getContext().getColorStateList(R.color.neutral_100));
+
+                buttonSales.setImageTintList(getContext().getColorStateList(R.color.blue_300));
+                buttonStock.setImageTintList(getContext().getColorStateList(R.color.blue_300));
+
+                root.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.blue_400));
+
+
                 break;
             case SALES:
                 buttonSales.setImageTintList(getContext().getColorStateList(R.color.blue_400));
@@ -64,6 +77,8 @@ public class AppNavigationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_app_navigation, container, false);
+
+        root = view.findViewById(R.id.fragment_appnavigation_constraint_root);
 
         buttonHome = view.findViewById(R.id.fragment_appnavigation_button_home);
         buttonSales = view.findViewById(R.id.fragment_appnavigation_button_sales);

7ae6fed33d19e2d2358dbe2cccca7e1f30686fa0
Dale Bezolli <deilbezolli@gmail.com>
2023-05-08 23:03:01 +0300

    Set matching colors when StockRecyclerViewAdapter generates is used in HomeActivity

--- app/src/main/java/com/example/shopmanager/Stocks/StockRecyclerViewAdapter.java
+++ app/src/main/java/com/example/shopmanager/Stocks/StockRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package com.example.shopmanager.Stocks;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.shopmanager.Home.HomeActivity;
 import com.example.shopmanager.MainActivity;
 import com.example.shopmanager.R;
 
@@ -22,6 +24,7 @@ import java.util.stream.Collectors;
 public class StockRecyclerViewAdapter extends RecyclerView.Adapter<StockRecyclerViewAdapter.ViewHolder> {
 
     private static final int SIZES_DISPLAY_COUNT = 3;
+    boolean isDisplayedInHome;
 
     Context context;
     ArrayList<StockDisplayModel> stockModels;
@@ -39,6 +42,8 @@ public class StockRecyclerViewAdapter extends RecyclerView.Adapter<StockRecycler
         this.context = context;
         this.stockModels = stock;
         this.onClickInterface = onClickInterface;
+
+        isDisplayedInHome = (Activity) context instanceof HomeActivity;
     }
 
     public void filterStock(String newText){
@@ -58,6 +63,22 @@ public class StockRecyclerViewAdapter extends RecyclerView.Adapter<StockRecycler
     @Override
     public void onBindViewHolder(@NonNull StockRecyclerViewAdapter.ViewHolder holder, int position) {
         StockDisplayModel element = stockModels.get(position);
+        if(isDisplayedInHome) {
+            holder.name.setTextColor(ContextCompat.getColorStateList(context, R.color.neutral_100));
+
+            holder.category.setTextColor(ContextCompat.getColorStateList(context, R.color.blue_400));
+            holder.category.setBackgroundTintList(ContextCompat.getColorStateList(context, R.color.neutral_100));
+
+            holder.currentPrice.setTextColor(ContextCompat.getColorStateList(context, R.color.neutral_100));
+            holder.originalPrice.setTextColor(ContextCompat.getColorStateList(context, R.color.blue_300));
+
+            holder.discountPercent.setTextColor(ContextCompat.getColorStateList(context, R.color.blue_400));
+            holder.discountPercent.setBackgroundTintList(ContextCompat.getColorStateList(context, R.color.neutral_100));
+
+            holder.totalStock.setTextColor(ContextCompat.getColorStateList(context, R.color.neutral_100));
+            holder.totalStockIcon.setImageTintList(ContextCompat.getColorStateList(context, R.color.neutral_100));
+            holder.totalStockLabel.setTextColor(ContextCompat.getColorStateList(context, R.color.blue_300));
+        }
 
         holder.name.setText(new StringBuilder().append(element.getBrand()).append(" - ").append(element.getName()));
         holder.category.setText(element.getCategory());
@@ -81,15 +102,20 @@ public class StockRecyclerViewAdapter extends RecyclerView.Adapter<StockRecycler
         int sizesCount = Math.min(element.getSizes().size(), SIZES_DISPLAY_COUNT);
         Object[] keys = element.getSizes().keySet().toArray();
         for(int i = 0; i < sizesCount; i++) {
-            holder.sizesList.addView(createSizeElement((String)keys[i], element.getSizes().get(keys[i])));
+            holder.sizesList.addView(createSizeElement((String)keys[i], element.getSizes().get(keys[i]), isDisplayedInHome));
         }
 
         if(element.getSizes().size() > SIZES_DISPLAY_COUNT) {
             LinearLayout moreView = new LinearLayout(context);
             ImageView moreDotsView = new ImageView(context);
-            moreDotsView.setImageResource(R.drawable.icon_dots);
 
-            moreView.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_full_blue));
+            int primaryColor = isDisplayedInHome ? R.color.blue_300 : R.color.neutral_300;
+            int backgroundElement = isDisplayedInHome ? R.drawable.background_rounded_full_white : R.drawable.background_rounded_full_blue;
+
+            moreDotsView.setImageResource(R.drawable.icon_dots);
+            moreDotsView.setImageTintList(ContextCompat.getColorStateList(context, primaryColor));
+
+            moreView.setBackground(ContextCompat.getDrawable(context, backgroundElement));
             moreView.setPadding(8, 7, 8, 7);
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             params.rightMargin = 8;
@@ -117,6 +143,8 @@ public class StockRecyclerViewAdapter extends RecyclerView.Adapter<StockRecycler
 
         LinearLayout sizesList;
         TextView totalStock;
+        ImageView totalStockIcon;
+        TextView totalStockLabel;
 
         public ViewHolder(@NonNull View view, StockOnClickInterface onClickInterface) {
             super(view);
@@ -132,6 +160,8 @@ public class StockRecyclerViewAdapter extends RecyclerView.Adapter<StockRecycler
 
             sizesList = view.findViewById(R.id.recyclerview_stock_list_sizes);
             totalStock = view.findViewById(R.id.recyclerview_stock_text_totalstock);
+            totalStockIcon = view.findViewById(R.id.recyclerview_stock_image_totalstock);
+            totalStockLabel = view.findViewById(R.id.recyclerview_stock_text_label_totalstock);
 
             view.setOnClickListener(v -> {
                 if(onClickInterface == null) return;
@@ -142,12 +172,16 @@ public class StockRecyclerViewAdapter extends RecyclerView.Adapter<StockRecycler
         }
     }
 
-    public View createSizeElement(String size, int amount) {
+    public View createSizeElement(String size, int amount, boolean isDisplayedInHome) {
         LinearLayout stockItemView = new LinearLayout(context);
         TextView sizeView = new TextView(context);
         TextView amountView = new TextView(context);
 
-        stockItemView.setBackground(ContextCompat.getDrawable(context, R.drawable.background_rounded_full_blue));
+        int primaryColor = isDisplayedInHome ? R.color.blue_400 : R.color.neutral_100;
+        int secondaryColor = isDisplayedInHome ? R.color.neutral_300 : R.color.blue_300;
+        int backgroundElement = isDisplayedInHome ? R.drawable.background_rounded_full_white : R.drawable.background_rounded_full_blue;
+
+        stockItemView.setBackground(ContextCompat.getDrawable(context, backgroundElement));
         stockItemView.setPadding(32, 6, 32, 6);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         params.rightMargin = 8;
@@ -157,13 +191,13 @@ public class StockRecyclerViewAdapter extends RecyclerView.Adapter<StockRecycler
 
         sizeView.setText(size);
         sizeView.setTypeface(ResourcesCompat.getFont(context, R.font.poppins_semibold));
-        sizeView.setTextColor(ContextCompat.getColor(context, R.color.neutral_100));
+        sizeView.setTextColor(ContextCompat.getColor(context, primaryColor));
         sizeView.setTextSize(8);
         sizeView.setIncludeFontPadding(false);
 
         amountView.setText(new StringBuilder().append(" - ").append(amount));
         amountView.setTypeface(ResourcesCompat.getFont(context, R.font.poppins_semibold));
-        amountView.setTextColor(ContextCompat.getColor(context, R.color.blue_300));
+        amountView.setTextColor(ContextCompat.getColor(context, secondaryColor));
         amountView.setTextSize(8);
         amountView.setIncludeFontPadding(false);
 
